f1e632054a3e6e8dfc1aafcecc5519679751ce85
Basit Ali Khan <bkghori@gmail.com>
2026-02-24 14:37:34 +0500

    Clone Changes..

--- SmartAssistant.App/obj/Debug/net9.0-ios/iossimulator-x64/SmartAssistant.App.AssemblyInfo.cs
+++ SmartAssistant.App/obj/Debug/net9.0-ios/iossimulator-x64/SmartAssistant.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAssistant.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+25039b6eb874491057f777db049c1e3bafded4f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAssistant.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAssistant.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
