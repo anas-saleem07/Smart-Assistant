25039b6eb874491057f777db049c1e3bafded4f4
Basit Ali Khan <bkghori@gmail.com>
2026-02-24 14:31:40 +0500

    Revert "Fix Issues"
    This reverts commit 27a448454e6f60307349802bd4f8672df5b9d40e.

--- SmartAssistant.App/obj/Debug/net9.0-android/SmartAssistant.App.AssemblyInfo.cs
+++ SmartAssistant.App/obj/Debug/net9.0-android/SmartAssistant.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAssistant.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+31368735a6a696aa567d170765c53e4e8e3be3a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAssistant.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAssistant.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
